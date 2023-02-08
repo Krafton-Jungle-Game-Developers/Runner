f9e5e9359f82080d9e0f408d95a22c28eb2fbced
remerer <remerer@gmail.com>
2023-02-09 02:22:07 +0900

    edit(todo)

--- Runner.Unity/Assets/Scripts/PlayerCameraEffect.cs
+++ Runner.Unity/Assets/Scripts/PlayerCameraEffect.cs
@@ -8,35 +8,15 @@ using UnityEngine.Rendering.Universal;
 
 public class PlayerCameraEffect : MonoBehaviour
 {
+    public Camera playerCamera;
     private Rigidbody rb;
 
-    public Camera playerCamera;
-
-    public float fov = 60f;
-    public bool invertCamera = false;
-    public bool cameraCanMove = true;
-    public float mouseSensitivity = 2f;
-    public float maxLookAngle = 50f;
-
-    // Crosshair
-    public bool lockCursor = true;
-    public bool crosshair = true;
-    public Sprite crosshairImage;
-    public Color crosshairColor = Color.white;
-
-    // Internal Variables
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
-    private Image crosshairObject;
-
     //Post Process
-    public bool enableZoom = true;
-    public bool holdToZoom = false;
-    public float zoomFOV = 30f;
-    public float zoomStepTime = 5f;
-
-    public float PlayerHorizontalSpeed = 0f;
+    public float playerHorizontalSpeed = 0f;
     public float MaxFOV = 110f;
+    public float baseFOV = 60f;
+    public float nowFOV = 60f;
+    public float maxFOV = 80;
     public UniversalAdditionalCameraData UAC;
 
     void Awake()
@@ -48,7 +28,6 @@ public class PlayerCameraEffect : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        crosshairObject = GetComponentInChildren<Image>();
     }
 
     // Update is called once per frame
@@ -60,36 +39,25 @@ public class PlayerCameraEffect : MonoBehaviour
     // Player Camera Effect (React by speed)
     private void CameraEffect()
     {
-        if (cameraCanMove)
+        playerHorizontalSpeed = new Vector3(rb.velocity.x, 0.0f, rb.velocity.z).magnitude;
+        if (playerHorizontalSpeed * 10 > baseFOV && playerHorizontalSpeed * 10 < maxFOV)
         {
-            yaw = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivity;
-
-            if (!invertCamera)
-            {
-                pitch -= mouseSensitivity * Input.GetAxis("Mouse Y");
-            }
-            else
-            {
-                // Inverted Y
-                pitch += mouseSensitivity * Input.GetAxis("Mouse Y");
-            }
-
-            // Clamp pitch between lookAngle
-            pitch = Mathf.Clamp(pitch, -maxLookAngle, maxLookAngle);
-
-            transform.localEulerAngles = new Vector3(0, yaw, 0);
-            playerCamera.transform.localEulerAngles = new Vector3(pitch, 0, 0);
+            nowFOV = (playerHorizontalSpeed * 10 + baseFOV) / 2;
         }
+        else if (playerHorizontalSpeed * 10 < baseFOV)
+        {
+            nowFOV = baseFOV;
 
-        //Reactive FOV with Player's Velocity
-        PlayerHorizontalSpeed = new Vector3(rb.velocity.x, 0.0f, rb.velocity.z).magnitude;
-        playerCamera.fieldOfView = PlayerHorizontalSpeed * 10 < fov ? fov : PlayerHorizontalSpeed * 10 > MaxFOV ? MaxFOV : PlayerHorizontalSpeed * 10;
+        }
+        else
+        {
+            nowFOV = maxFOV;
+        }
+        playerCamera.fieldOfView = nowFOV;
 
-        if (PlayerHorizontalSpeed > 6f)
+        if (nowFOV >= 60f)
             UAC.renderPostProcessing = true;
         else
             UAC.renderPostProcessing = false;
-
-
     }
 }
