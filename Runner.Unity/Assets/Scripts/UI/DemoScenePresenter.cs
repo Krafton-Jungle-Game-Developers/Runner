cb6792105604ff5d38a197f5e35696fb702d765e
SeungHu Lee <gadlsh12081208@gmail.com>
2023-02-09 11:47:10 +0900

    Add Demo Scene Presenter

---
+++ Runner.Unity/Assets/Scripts/UI/DemoScenePresenter.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+
+namespace Runner.UI
+{
+    public class DemoScenePresenter : MonoBehaviour
+    {
+        [SerializeField] private Image fadePanelImage;
+
+        private async UniTaskVoid Awake()
+        {
+            await fadePanelImage.DOFade(0f, 0.5f);
+        }
+    }
+}
