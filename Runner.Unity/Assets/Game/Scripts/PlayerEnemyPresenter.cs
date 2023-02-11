af1e0f5304f6c7984632ef7ad9233f63873a3b02
SeungHu Lee <gadlsh12081208@gmail.com>
2023-02-12 03:44:21 +0900

    Add scripts

--- Runner.Unity/Assets/Game/Scripts/PlayerEnemyPresenter.cs
+++ Runner.Unity/Assets/Game/Scripts/PlayerEnemyPresenter.cs
@@ -1,9 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 namespace Runner.Game
 {
     public class PlayerEnemyPresenter : MonoBehaviour
     {
+        [SerializeField] private List<EnemyModel> enemyModels = new();
 
+        private void Awake()
+        {
+            foreach (var enemyModel in enemyModels)
+            {
+                enemyModel.OnEnemyBecameVisibleObservable.Subscribe(_ =>
+                {
+                    Debug.Log($"{enemyModel.gameObject.name} is Visible!");
+                }).AddTo(this);
+
+                enemyModel.OnEnemyBecameInvisibleObservable.Subscribe(_ =>
+                {
+                    Debug.Log($"{enemyModel.gameObject.name} is Invisible!");
+                }).AddTo(this);
+            }
+        }
     }
 }
