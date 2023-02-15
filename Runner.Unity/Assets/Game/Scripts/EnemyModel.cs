749bd27b2a51967cef16b551a2aec591907f6649
SeungHu Lee <gadlsh12081208@gmail.com>
2023-02-16 00:12:30 +0900

    Basic Execute

--- Runner.Unity/Assets/Game/Scripts/EnemyModel.cs
+++ Runner.Unity/Assets/Game/Scripts/EnemyModel.cs
@@ -10,29 +10,28 @@ namespace Runner.Game
     public class EnemyModel : MonoBehaviour
     {
         private SkinnedMeshRenderer _enemyRenderer;
-        private PlayerAbilityController _player;
-        private HUDPresenter _HUD;
+        private Animator _animator;
+        private int _dieTrigger = Animator.StringToHash("Die");
 
-        [Inject]
-        private void Construct(PlayerAbilityController player, HUDPresenter HUD)
-        {
-            _player = player;
-            _HUD = HUD;
-        }
-        
-        private ReactiveProperty<bool> _isDead;
-        public IReactiveProperty<bool> IsDead => _isDead;
-        private IObservable<Vector3> _onEnemyBecameVisibleObservable;
-        public IObservable<Vector3> OnEnemyBecameVisibleObservable => _onEnemyBecameVisibleObservable;
+        public BoolReactiveProperty IsVisible;
+        private bool _isDead = false;
+        public bool IsDead => _isDead;
+        private IObservable<EnemyModel> _onEnemyExecutableObservable;
+        public IObservable<EnemyModel> OnEnemyExecutableObservable => _onEnemyExecutableObservable;
         
         private void Awake()
         {
             _enemyRenderer = GetComponent<SkinnedMeshRenderer>();
-            _onEnemyBecameVisibleObservable = Observable.Interval(TimeSpan.FromSeconds(0.1f))
-                                                        .TakeWhile(_ => _enemyRenderer.isVisible)
-                                                        .Repeat()
-                                                        .Select(_ => transform.position);
-            _isDead = new(false);
+            _animator = GetComponent<Animator>();
+            IsVisible = new(false);
+        }
+
+        private void OnBecameVisible() => IsVisible.Value = true;
+        private void OnBecameInvisible() => IsVisible.Value = false;
+
+        private void Die()
+        {
+            _animator.SetTrigger(_dieTrigger);
         }
     }
 }
